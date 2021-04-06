7cd2151a9cbba914617c532791190a8efc22a3ec
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-06 15:57:37 -0400

    wrote put request

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -44,6 +44,30 @@ router.get("/:id/comments", (req, res)=>{
     })
 })
 
+//[PUT] Specific Post
+
+router.put("/:id", (req, res)=>{
+    const { id }=req.params;
+    const post = req.body;
+
+    Posts.update(id, post )
+    .then((numberOfRecs)=>{
+        if (numberOfRecs){
+            return Posts.findById(id)
+        } else {
+            res.status(404).json({message: "Post Not Found"});
+        }
+    })
+    .then((post)=>{
+        if (post){
+            res.status(200).json(post);
+        } 
+    })
+    .catch((err)=>{
+        res.status(500).json({message: err.message})
+    })
+})
+
 
 //EXPORTS
 
