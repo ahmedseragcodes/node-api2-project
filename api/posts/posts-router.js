3e002a06c94eaddab1c4723524dff2ff260df796
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-06 14:45:50 -0400

    installed dependencies, setup basic file structure, setup imports for router and server files

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,15 @@
 // implement your posts router here
+
+//IMPORTS
+
+const Posts = require("./posts-model");
+const express = require("express");
+const router = express.Router();
+
+
+
+
+
+//EXPORTS
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,18 @@
 // implement your server here
 // require your posts router and connect it here
+
+//IMPORTS 
+const express = require ("express");
+const postsRouter = require("./posts/posts-router");
+
+//INSTANCE
+
+const server = express();
+
+//GLOBAL MIDDLEWARE
+
+server.use(express.json());
+
+server.use("/api/posts", postsRouter);
+
+module.exports = server;
