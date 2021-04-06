05483cbcfd20b2b5c34b37cca63f7672d0163234
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-06 15:21:30 -0400

    wrote get requests

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -6,8 +6,43 @@ const Posts = require("./posts-model");
 const express = require("express");
 const router = express.Router();
 
+//ENDPOINTS
 
+//[GET] ALL POSTS
 
+router.get("/", (req, res)=>{
+    Posts.find()
+    .then((post)=>{
+        res.status(200).json(post);
+    })
+    .catch((err)=>{
+        res.status(500).json({message: err.message})
+    })
+})
+
+//[GET] SPECIFIC POST
+router.get("/:id", (req, res)=>{
+    const { id }=req.params;
+    Posts.findById(id)
+    .then((post)=>{
+        res.status(200).json(post);
+    })
+    .catch((err)=>{
+        res.status(500).json({message: err.message})
+    })
+})
+
+//[GET] Comments On Specific Post
+router.get("/:id/comments", (req, res)=>{
+    const { id }=req.params;
+    Posts.findPostComments(id)
+    .then((postComments)=>{
+        res.status(200).json(postComments);
+    })
+    .catch((err)=>{
+        res.status(500).json({message: err.message})
+    })
+})
 
 
 //EXPORTS
