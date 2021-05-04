a7374d03a061490ae5240c46e2e447cbe6bd4c93
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-04 12:37:06 -0400

    wrote get posts and get post by id routers along with necessary validation

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,42 @@
-// implement your posts router here
+const express = require("express");
+const Posts = require("./posts-model");
+
+const router = express.Router();
+
+//ENDPOINTS
+
+//[GET] All Posts
+
+router.get("/", (req, res, next)=>{
+
+    Posts.find()
+    .then((allPosts)=>{
+        res.status(200).json(allPosts);
+    })
+    .catch((err)=>{
+        res.status(500).json({message: "The posts information could not be retrieved"});
+    })
+});
+
+//[GET] Post By Id
+
+router.get("/:id", (req, res, next)=>{
+    
+    const { id } = req.params;
+    
+    Posts.findById(id)
+    .then((specificPost)=>{
+        if(!specificPost){
+            res.status(404).json({message: "The post with the specified ID does not exist"});
+        } else {
+            res.status(200).json(specificPost);
+        }
+    })
+    .catch((err)=>{
+        res.status(500).json({message: "The post information could not be retrieved"});
+    })
+});
+
+
+
+module.exports = router;
